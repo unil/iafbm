e8f64a93b7222b1002b7a1f69ac4de5d500e9af2
dcorpata <dcorpata@unil.(none)>
2011-04-12 10:38:53 +0200

    ApiFront: modified behaviour for fitting ExtJs Ajax requests

--- iafbm/fronts/api.php
+++ iafbm/fronts/api.php
@@ -5,6 +5,8 @@ class ApiFront extends xApiFront {
 
     function __construct($params = null) {
         parent::__construct($params);
+        // Sets the called method according the HTTP Request Verb if no method specified
+        if (!@$this->params['xmethod']) $this->params['xmethod'] = @$this->http['method'];
         $this->prepare();
     }
 
@@ -12,13 +14,13 @@ class ApiFront extends xApiFront {
      * Merges the HTTP Request body paramters with the the instance parameters.
      */
     function prepare() {
-        // Sets the called method according the HTTP Request Verb
-        // if no method specified
-        if (!@$this->params['xmethod']) $this->params['xmethod'] = @$this->http['method'];
         // Merges HTTP Request body with the instance parameters
         $body = $this->get_request_body();
         $params = $this->decode($body);
-        $params = @array_shift($params); // Removes JSON root cell: this will not work all the time and should be used only for post+get requests
+        // Removes JSON root cell post+get requests (According Ext.data.JsonWriter behaviour)
+        if (in_array(strtolower($this->params['xmethod']), array('put', 'post'))) {
+            $params = @array_shift($params);
+        }
         $this->params = xUtil::array_merge($this->params, xUtil::arrize($params));
     }
 
