d799c155e93b37160b01ac55e2dcb39d00bcbfd9
d@mien.ch <dcorpata@unil.ch>
2011-04-26 11:08:07 +0200

    fronts/api: overriden xApiFront methods_mapping to fit ExtJS HTTP Methods usage"

--- iafbm/fronts/api.php
+++ iafbm/fronts/api.php
@@ -2,6 +2,15 @@
 
 class ApiFront extends xApiFront {
 
+    /**
+     * @see xFront::$method_mapping
+     */
+    var $method_mapping = array(
+        'GET' => 'get',
+        'POST' => 'get',
+        'PUT' => 'post',
+        'DELETE' => 'delete'
+    );
 
     function __construct($params = null) {
         parent::__construct($params);
