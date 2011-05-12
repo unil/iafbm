d7e4d738946ea3b11dc76aa25743953e4d08ce2e
d@mien.ch <dcorpata@unil.ch>
2011-05-12 17:00:02 +0200

    Added a sandbox controller for sandboxing

---
+++ iafbm/controllers/sandbox.php
@@ -0,0 +1,41 @@
+<?php
+
+class SandboxController extends xWebController {
+
+    function defaultAction() {
+        foreach (get_class_methods($this) as $method)
+            if (substr($method, -strlen('Action')) == 'Action')
+                $actions[] = substr($method, 0, -strlen('Action'));
+        foreach ($actions as $action)
+            @$html .= "<a href=\"".xUtil::url("sandbox/do/{$action}")."\">{$action}</a><br>";
+        return $html;
+    }
+
+    function selectiongridAction() {
+return <<<EOL
+<div id="target"></div>
+<script>
+Ext.onReady(function() {
+
+c = new Ext.ia.selectiongrid.Panel({
+    renderTo: 'target',
+    frame: true,
+    title: 'Test',
+    width: 500,
+    height: 300,
+    combo: {
+        store: new iafbm.store.Personne(),
+    },
+    grid: {
+        store: new iafbm.store.Membre(),
+        columns: iafbm.columns.Membre
+    }
+});
+
+});
+</script>
+
+EOL;
+    }
+
+}
