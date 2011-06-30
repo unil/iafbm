b85be6b8ae419bd0ef15450a31f286e961a72d84
Damien Corpataux <d@mien.ch>
2011-06-30 14:46:23 +0200

    SQL: reorganised SQL generation files numeric prefix

---
+++ sql/112_commissions_etats.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS commissions_etats;
+CREATE TABLE commissions_etats (
+    id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `commissions_etats` (nom, description) VALUES ('En cours', 'Commission en cours');
+INSERT INTO `commissions_etats` (nom, description) VALUES ('Clôturé', 'Commission clôturée');
+INSERT INTO `commissions_etats` (nom, description) VALUES ('En suspens', 'Commission en suspens');
