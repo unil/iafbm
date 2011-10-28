87efa10260369f2e057a7a454cabc05c930fc647
Damien Corpataux <d@mien.ch>
2011-10-28 11:46:03 +0200

    SQL: Updated populate.sh to set the development database name (hardcoded)

--- sql/populate.sh
+++ sql/populate.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 MYSQL_USER=root
+TMP_FILE=merged_sed.sql
 
 # Merges all sql files into one single file
 ./merge.sh
 
 # Imports the merged file
 echo Connecting to database using user \'$MYSQL_USER\'
-mysql -v -u $MYSQL_USER -p --default-character-set=utf8 < merged.sql
+cat merged.sql | sed s/{db-name}/iafbm/ > $TMP_FILE
+mysql -v -u $MYSQL_USER -p --default-character-set=utf8 < $TMP_FILE
+rm $TMP_FILE
 
 # Cleans the merged file
 #rm $MERGE_FILE
