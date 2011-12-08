d8ce754ca53ad87300f6d2f46642a822929d8240
Damien Corpataux <d@mien.ch>
2011-12-08 17:58:30 +0100

    SQL: Ordered commissions_etats according ids

--- sql/112_commissions_etats.sql
+++ sql/112_commissions_etats.sql
@@ -6,6 +6,6 @@ CREATE TABLE commissions_etats (
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `commissions_etats` (nom, description) VALUES ('En cours', 'Commission en cours');
-INSERT INTO `commissions_etats` (nom, description) VALUES ('Clôturé', 'Commission clôturée');
-INSERT INTO `commissions_etats` (nom, description) VALUES ('En suspens', 'Commission en suspens');
+INSERT INTO `commissions_etats` (id, nom, description) VALUES (1, 'En cours', 'Commission en cours');
+INSERT INTO `commissions_etats` (id, nom, description) VALUES (2, 'En suspens', 'Commission en suspens');
+INSERT INTO `commissions_etats` (id, nom, description) VALUES (3, 'Clôturé', 'Commission clôturée');
