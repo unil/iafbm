cd91be7c61c1c11f323c1161d86b656c58bffb86
Damien Corpataux <d@mien.ch>
2012-01-16 18:28:14 +0100

    UnitTests: Fixed project-specific phpunit.php

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -1,10 +1,24 @@
 <?php
 
-$lib = '../iafbm/lib/PhpUnit';
+// Setups PHPUnit libs paths
+$phpunit = '../iafbm/lib/PhpUnit';
 $pear = '../iafbm/lib/PEAR';
-
 set_include_path(get_include_path() . PATH_SEPARATOR . $pear);
+set_include_path(get_include_path() . PATH_SEPARATOR . $phpunit);
 
-require_once("{$lib}/phpunit.php");
+
+// Project specific TestCase class
+class PHPUnit_Iafbm {
+
+    public static function setupBootstrap() {
+        require_once('../iafbm/public/Bootstrap.php');
+        return new Bootstrap();
+    }
+
+}
+
+
+// Runs PHPUnit
+require_once("{$phpunit}/phpunit.php");
 
 ?>
