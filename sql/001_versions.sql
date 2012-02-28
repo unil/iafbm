da67231e67fb9c82bb5a5ce18093b80d5e5abd7a
Damien Corpataux <d@mien.ch>
2012-02-28 15:13:07 +0100

    SQL: Fixed version.id, version_data.id type

--- sql/001_versions.sql
+++ sql/001_versions.sql
@@ -1,27 +1,28 @@
 DROP TABLE IF EXISTS versions;
 CREATE TABLE versions (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     creator VARCHAR(255) NOT NULL,
-    table_name varchar(255) NOT NULL,
-    model_name varchar(255) NOT NULL,
-    id_field_name varchar(255) NOT NULL,
+    table_name VARCHAR(255) NOT NULL,
+    model_name VARCHAR(255) NOT NULL,
+    id_field_name VARCHAR(255) NOT NULL,
     id_field_value INT NOT NULL,
-    operation varchar(8) NOT NULL,
-    commentaire varchar(255),
+    operation VARCHAR(8) NOT NULL,
+    commentaire VARCHAR(255),
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- Creates an empty initial version
+INSERT INTO versions (table_name, model_name, id_field_name, id_field_value, operation, commentaire) VALUES ('*', '*', '*', 0, 'tag', 'Initial version (empty storage)');
+
+
 DROP TABLE IF EXISTS versions_data;
 CREATE TABLE versions_data (
-    id INT NOT NULL AUTO_INCREMENT,
-    version_id INT NOT NULL,
-    field_name varchar(255) NOT NULL,
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    version_id INT UNSIGNED NOT NULL,
+    field_name VARCHAR(255) NOT NULL,
     old_value TEXT,
     new_value TEXT,
     PRIMARY KEY (id),
     FOREIGN KEY (version_id) REFERENCES versions(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Creates an empty initial version
-INSERT INTO versions (table_name, model_name, id_field_name, id_field_value, operation, commentaire) VALUES ('*', '*', '*', 0, 'tag', 'Initial version (empty storage)');
