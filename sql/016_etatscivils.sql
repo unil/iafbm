80a893fcd162a26161701646f0fbb073c9544140
Damien Corpataux <d@mien.ch>
2012-03-12 11:08:02 +0100

    SQL: Etatscivils.nom length to 100 chars (closes #127)

--- sql/016_etatscivils.sql
+++ sql/016_etatscivils.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS etatscivils;
 CREATE TABLE etatscivils (
     id INT NOT NULL AUTO_INCREMENT,
     actif BOOLEAN NOT NULL DEFAULT true,
-    nom VARCHAR(16) NOT NULL,
+    nom VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
