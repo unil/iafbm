b305fdd33e40fdbcbaea368cdefc26cb56f33142
Damien Corpataux <d@mien.ch>
2012-04-12 16:58:59 +0200

    Home controller: Cleaned unused metadata setup

--- iafbm/controllers/home.php
+++ iafbm/controllers/home.php
@@ -2,11 +2,6 @@
 
 class HomeController extends iaWebController {
 
-    function __construct($params=array()) {
-        parent::__construct($params);
-        $this->add_meta('navigation/highlight', 'home');
-    }
-
     function defaultAction() {
         return xView::load('home/home')->render();
     }
