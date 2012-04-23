bde22266c13b7fab82d427b6af32bcf4b106351e
Damien Corpataux <d@mien.ch>
2012-04-23 17:43:52 +0200

    Candidat controller: Implemented delete (closes #173)

--- iafbm/controllers/candidats.php
+++ iafbm/controllers/candidats.php
@@ -53,6 +53,53 @@ class CandidatsController extends AbstractCommissionController {
     }
 
     /**
+     * Deletes 'candidat':
+     * - Cascades delete on 'candidat_formation'
+     * - Nulls 'commission_travail.*_loco'
+     * - Nulls 'commission_finalisation.candidat_id'
+     */
+    function delete() {
+        if (!in_array('delete', $this->allow)) throw new xException("Method not allowed", 403);
+        $t = new xTransaction();
+        $t->start();
+        // Determines candidat & commission id
+        $id_candidat = $this->params['id'];
+        $id_commission = xModel::load('candidat', array('id' => $id_candidat))->get(0);
+        $id_commission = $id_commission['commission_id'];
+        // Soft-deletes foreign 'candidat_formation' entity/ies
+        $ids = $this->_get_ids(
+            xModel::load('candidat_formation', array('candidat_id'=>$id_candidat))
+        );
+        foreach ($ids as $id) $t->execute(xModel::load('candidat_formation', array(
+            'id' => $id
+        )), 'delete');
+        // Nulls foreign 'commission_travail' reference
+        $t->execute(xModel::load('commission_travail', array(
+            'id' => $id_commission,
+            'primo_loco' => null,
+            'secondo_loco' => null,
+            'tertio_loco' => null
+        )), 'post');
+        // Nulls foreign 'commission_travail' reference
+        $t->execute(xModel::load('commission_finalisation', array(
+            'id' => $id_commission,
+            'candidat_id' => null
+        )), 'post');
+        // Soft-deletes actual entity (soft-delete)
+        $t->execute(xModel::load('candidat', array(
+            'id' => $id_candidat,
+            'actif' => 0
+        )), 'post');
+        return $t->end();
+    }
+    protected function _get_ids(xModel $model) {
+        $r = $model->get();
+        $ids = array();
+        foreach ($r as $item) $ids[] = $item[$model->primary()];
+        return $ids;
+    }
+
+    /**
      * @see PersonnesController
      */
     protected function transform_params() {
