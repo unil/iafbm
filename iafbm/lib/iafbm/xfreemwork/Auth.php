a9dd25ba4619631cdf1b2f7c5b91186bc25be95e
Damien Corpataux <d@mien.ch>
2012-05-08 09:46:39 +0200

    Auth: Created roles definition data structure

--- iafbm/lib/iafbm/xfreemwork/Auth.php
+++ iafbm/lib/iafbm/xfreemwork/Auth.php
@@ -1,6 +1,35 @@
 <?php
 
 class iaAuth extends xAuth {
+   /**
+    * Persmissions configuration:
+    * - Each group permission is read sequentially.
+    * - When a user is in multiple groups,
+    * array(
+    *     'role' => array(
+    *         'models' => array(
+    *             '*' => 'R', // Allow reading of all models
+    *             'person' => 'crud',
+    *             'address' => 'cru',
+    *             'email' => null // Revokes all action on email model
+    *         )
+    *     )
+    * )
+    * @var array
+    */
+    protected $permissions = array(
+        'fbm-iafbm-g' => array(
+            'models' => array(
+                '*' => 'R',
+                'candidat' => null,
+            )
+        ),
+        'fbm-iafbm-releve-g, fbm-iafbm-admin-g' => array(
+            'models' => array(
+                '*' => 'CRUD'
+            )
+        )
+    );
 
     protected $role_separator = ';';
 
@@ -19,5 +48,4 @@ class iaAuth extends xAuth {
         $roles = @$_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF'];
         $this->set($username, $roles);
     }
-
 }
