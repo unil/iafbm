c4badd9e958de5e33a2978001f746545c36e5426
Damien Corpataux <d@mien.ch>
2012-05-11 13:35:59 +0200

    Auth: Unittests: Created iaAuth unittests (#180)

--- tests/units/AuthenticationTest.php
+++ tests/units/AuthenticationTest.php
@@ -1,127 +1,115 @@
 <?php
 
+require_once(__DIR__.'/../../iafbm/lib/xfreemwork/lib/lib/Util/Auth.php');
+require_once(__DIR__.'/../../iafbm/lib/iafbm/xfreemwork/Auth.php');
+require_once(__DIR__.'/../../iafbm/lib/xfreemwork/lib/lib/Core/Bootstrap.php');
+require_once(__DIR__.'/../../iafbm/public/Bootstrap.php');
+
+/**
+ * Unittesing-specific iaAuth class.
+ * Defines a static $permission.
+ */
+class iaTestAuth extends iaAuth {
+    protected $permissions = array(
+        'multiple , roles, canonalization' => array(
+            'models' => array(
+                'somemodel' => 'CRU'
+            )
+        ),
+        'wildcards' => array(
+            'models' => array(
+                '*' => 'R',
+                'candidat' => null
+            )
+        ),
+    );
+
+    function get_permissions() {
+        return $this->permissions;
+    }
+}
+
+/**
+ * Unittesing-specific Bootstrap.
+ * Setups iaTestAuth as xContext::$auth.
+ */
+class iaTestAuthBootstrap extends Bootstrap {
+    function setup_auth() {
+        xContext::$log->log("Setting up iaTestAuth", $this);
+        xContext::$auth = new iaTestAuth();
+    }
+}
+
 /**
  * Tests iaAuth class.
  */
-class AuthTest extends iaPHPUnit_Framework_TestCase {
+class AuthenticationTest extends iaPHPUnit_Framework_TestCase {
 
-    protected $shib_username = 'username';
-    protected $shib_org = 'organisation';
-    protected $shib_roles = 'fbm-iafbm-g';
-
-    /**
-     * @see iaPHPUnit_Framework_TestCase::setUp()
-     */
     function setUp() {
+        new iaTestAuthBootstrap();
+    }
+
+    protected function set_shibboleth($username, $org, $roles) {
         // Simulates Shibboleth server information
-        $_SERVER['HTTP_SHIB_PERSON_UID'] = $this->shib_username;
-        $_SERVER['HTTP_SHIB_SWISSEP_HOMEORGANIZATION'] = $this->shib_org;
-        $_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF'] = $this->shib_roles;
-        // Parent setUp() creates Bootstrap and parses auth information
-        parent::setUp();
+        $_SERVER['HTTP_SHIB_PERSON_UID'] = $username;
+        $_SERVER['HTTP_SHIB_SWISSEP_HOMEORGANIZATION'] = $org;
+        $_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF'] = $roles;
         // Reparses auth information
         xContext::$auth->set_from_aai();
     }
 
     function test_auth_info_from_shibboleth() {
         $auth = xContext::$auth;
-        $username = "{$this->shib_username}@{$this->shib_org}";
-        $roles = array_map('trim', explode(';', $this->shib_roles));
+        // Auth test-data set
+        $shib_username = 'username';
+        $shib_org = 'organisation';
+        $shib_roles = 'fbm-iafbm-g ; abc;def; hij;klm ';
+        // Simulates Shibboleth server information
+        $this->set_shibboleth($shib_username, $shib_org, $shib_roles);
+        // Reparses auth information
+        xContext::$auth->set_from_aai();
+        // Tests stored auth information
+        $username = "{$shib_username}@{$shib_org}";
+        $roles = array_map('trim', explode(';', $shib_roles));
         $this->assertSame($username, $auth->username());
         $this->assertSame($roles, $auth->roles());
     }
 
-    function test_permissions_allroles() {
-        $permissions = array();
-        foreach (xModel::scan() as $model) {
-            $permissions[$model] = array('get', 'put', 'post', 'delete');
-        }
-        $this->do_test_permissions(
-            'fbm-iafbm-g;fbm-iafbm-releve-g;fbm-iafbm-admin-g',
-            $permissions
-        );
-    }
-
-    function test_permissions_fbm_iafbm_g() {
-        $permissions = array();
-        foreach (xModel::scan() as $model) {
-            if ($model == 'candidat') continue;  // Candidat model is disallowed
-            $permissions[$model] = array('get'); // Every other model is get allowed
-        }
-        $this->do_test_permissions(
-            'fbm-iafbm-g',
-            $permissions
-        );
-    }
-
-    function test_permissions_fbm_iafbm_releve_g() {
-        $permissions = array();
-        foreach (xModel::scan() as $model) {
-            $permissions[$model] = array('get', 'put', 'post', 'delete');
-        }
-        $this->do_test_permissions(
-            'fbm-iafbm-releve-g',
-            $permissions
-        );
-    }
-
-    /**
-     * Tests permissions on all models and operations combinaison,
-     * according the given $roles end expected $permissions.
-     * @param string Semicolon separated roles.
-     * @param array Expected permissions per model (eg: 'modelname' => array('get', 'put', 'post'))
-     */
-    protected function do_test_permissions($roles, $permissions) {
+    function test_canonalization_multiple_roles() {
         $auth = xContext::$auth;
-        $ops = array('get', 'put', 'post', 'delete');
-        // Simulates Shibboleth authentication
-        $_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF'] = $roles;
-        $auth->set_from_aai();
-        // Asserts allowed/disallowed operations per model
-        foreach ($permissions as $model => $operations) {
-            // Allowed operations
-            foreach ($operations as $operation) {
-                $this->assertAllowed($model, $operation);
-            }
-            // Disallowed operations
-            foreach (array_diff($ops, $operations) as $operation) {
-                $this->assertDisallowed($model, $operation);
+        $permissions = $auth->get_permissions();
+        // Only keeps roles to test (eg. it canonalizes)
+        $permissions = xUtil::filter_keys($permissions, array('multiple', 'roles', 'canonalization'));
+        foreach ($permissions as $role => $info) {
+            $models = $info['models'];
+            foreach ($models as $model => $operations) {
+                $this->assertSame('somemodel', $model);
+                $this->assertSame('CRU', $operations);
             }
         }
-        // Disallowed models (those are not in $permissions
-        // and all operations should be disallowed
-        $models = array_diff(xModel::scan(), array_keys($permissions));
-        foreach ($models as $model) {
-            foreach ($ops as $operation) {
-                $this->assertDisallowed($model, $operation);
+    }
+
+    function test_canonalization_models_wildcards() {
+        // Creates expected permissions
+        $expected = array();
+        foreach (xModel::scan() as $model) {
+            // Every model is 'get' allowed, except candidat
+            $expected[$model] = ($model != 'candidat') ? 'R' : null;
+        }
+        // Actual test
+        $auth = xContext::$auth;
+        $permissions = $auth->get_permissions();
+        // Only keeps role to test (eg. it has a wildcard)
+        $permissions = xUtil::filter_keys($permissions, array('wildcards'));
+        foreach ($permissions as $role => $info) {
+            $models = $info['models'];
+            foreach ($models as $model => $operations) {
+                $this->assertSame(
+                    $expected[$model],
+                    $operations,
+                    "Model $model"
+                );
             }
         }
-
-    }
-
-    /**
-     * Asserts that the given $model is allowed to execute $operation.
-     * @param string The model name.
-     * @param array The expected allowed operation.
-     */
-    protected function assertAllowed($model, $operation) {
-        $roles = implode(', ', xContext::$auth->roles());
-        $this->assertTrue(
-            xContext::$auth->is_allowed_model($model, $operation),
-            "Failed asserting that {$model}:{$operation} is allowed (role: {$roles})"
-        );
-    }
-
-    /**
-     * Asserts that the given $model is disallowed to execute $operation.
-     * @param string The model name.
-     * @param array The expected disallowed operation.
-     */
-    protected function assertDisallowed($model, $operation) {
-        $roles = implode(', ', xContext::$auth->roles());
-        $this->assertFalse(
-            xContext::$auth->is_allowed_model($model, $operation),
-            "Failed asserting that {$model}:{$operation} is disallowed (role: {$roles})"
-        );
     }
 }
