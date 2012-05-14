0e8929d27c9e447ba4500711603f9b76409b61c1
Damien Corpataux <d@mien.ch>
2012-05-14 12:42:15 +0200

    User: Fixed controller & view

--- iafbm/controllers/user.php
+++ iafbm/controllers/user.php
@@ -9,7 +9,32 @@ class UserController extends iaWebController {
     function detailAction() {
         $data = array(
             'username' => xContext::$auth->username(),
-            'roles' => xContext::$auth->roles()
+            'roles' => xContext::$auth->roles(),
+            'permissions' => xContext::$auth->user_permissions(),
+            'versions' => array(
+                'total' => xModel::load('version')->count(),
+                'count' => xModel::load('version', array(
+                    'creator' => xContext::$auth->username()
+                ))->count(),
+                'first' => xModel::load('version', array(
+                    'creator' => xContext::$auth->username(),
+                    'xorder_by' => 'created',
+                    'xorder' => 'ASC',
+                    'xlimit' => 1
+                ))->get(0),
+                'last' => xModel::load('version', array(
+                    'creator' => xContext::$auth->username(),
+                    'xorder_by' => 'created',
+                    'xorder' => 'DESC',
+                    'xlimit' => 1
+                ))->get(0)
+            ),
+            'modifications' => array(
+                'total' => xModel::load('version_data')->count(),
+                'count' => xModel::load('version_data', array(
+                    'version_creator' => xContext::$auth->username()
+                ))->count()
+            )
         );
         return xView::load('user/detail', $data)->render();
     }
