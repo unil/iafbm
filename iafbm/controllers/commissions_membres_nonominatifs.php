e278eccb016629ab52efea66363529e06ca27efa
Damien Corpataux <d@mien.ch>
2012-07-31 15:17:43 +0200

    Added model and migration script

---
+++ iafbm/controllers/commissions_membres_nonominatifs.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('commissions.php');
+
+class CommissionsMembresNonominatifsController extends AbstractCommissionController {
+
+    var $model = 'commission_membre_nonominatif';
+
+    var $sort_fields_substitutions = array(
+        'activite_id' => array(
+            'field' => 'activite_nom_nom',
+            'join' => 'activite,activite_nom'
+        ),
+        'rattachement_id' => 'rattachement_nom',
+        'commission_fonction_id' => 'commission_fonction_position',
+        // Personne:Commission Form:
+        // No need to define substitutions as long
+        // as CommissionMembreModel.xjoin contains all possible joins.
+        // Except for 'commission_etat_nom':
+        // we want to sort it on id field
+        'commission_etat_nom' => 'commission_etat_id'
+    );
+}
