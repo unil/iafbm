1d07375c748f47865140e5ab17fc602aeef9c3a9
Damien Corpataux <d@mien.ch>
2012-08-08 15:11:42 +0200

    Working version of commission_membre CSV export

--- iafbm/controllers/commissions_membres.php
+++ iafbm/controllers/commissions_membres.php
@@ -20,4 +20,34 @@ class CommissionsMembresController extends AbstractCommissionController {
         // we want to sort it on id field
         'commission_etat_nom' => 'commission_etat_id'
     );
+
+    function export() {
+        // TODO
+        // Fields: 'Dénomination', 'Fonction', 'Complément de fonction', 'Nom et prénom', 'Type adresse', 'Rue', 'NPA', 'Ville', 'Pays', 'Type téléphone', 'Indicatif', 'Numéro', 'Type email', 'Email'
+        $commission_id = @$this->params['id'];
+        if (!$commission_id) throw new xException("id parameter missing, please provide a 'commission' id", 400);
+        // Fetches 'commission_membre' rows
+        $data = xModel::load('commission_membre', array(
+            'commission_id' => $commission_id
+        ))->get();
+        // Adds versioned 'adresse' model row for each 'commission_membre' row
+        foreach ($data as &$d) {
+            $adresses = xModel::load('personne_adresse', array(
+                'personne_id' => $d['personne_id'],
+                'xversion' => $d['version_id']
+            ))->get();
+xUtil::pre($adresse);
+            // Discards adresses that are not set as default
+            // This has to be done after retrieval because of xversion
+            foreach ($adresses as $i => $a) if (!$a['default']) unset($adresses[$i]);
+            // Checks that only one default adresse exists
+            if (count($adresses) > 1) throw new xException(
+                "Personne id {$d['personne_id']} have multiple default adresses"
+            );
+            // Adds adresse fields to personne row
+            $adresse = array_shift($adresses);
+            foreach ($adresse as $field => $value) $d[$field] = $value;
+        }
+        return $data;
+    }
 }
