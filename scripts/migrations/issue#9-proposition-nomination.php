40f8e7e551b2bccd12f6257be4b6763180636bb3
Damien Corpataux <d@mien.ch>
2012-08-27 17:12:43 +0200

    Created Proposition nomination migration script (relates #9)

--- scripts/migrations/issue#9-proposition-nomination.php
+++ scripts/migrations/issue#9-proposition-nomination.php
@@ -10,34 +10,18 @@ class iafbmIssue9 extends iafbmScript {
             throw new Exception('This script has already run');
         }
         //
-        $this->create_fields();
-    }
-
-    function create_fields() {
-        $statements = array(
-            TODO
-            'ALTER TABLE iafbm.commissions_membres ADD COLUMN fonction_complement TEXT AFTER commission_fonction_id',
-            'ALTER TABLE iafbm.commissions_membres ADD COLUMN personne_denomination_id INT AFTER fonction_complement',
-            'ALTER TABLE iafbm.commissions_membres ADD FOREIGN KEY (personne_denomination_id) REFERENCES personnes_denominations(id)',
-        );
-        $t = new xTransaction();
-        $t->start();
-        foreach ($statements as $statement) xModel::q($statement);
-        $t->end();
+        $this->execute_sql_file('191_commissions_propositions_nominations.sql');
     }
 
     /**
-     * Returns true if the fields to create already exist.
+     * Returns true if the table to create already exists.
      * This means the script has already been run on this instance.
      */
     function already_run() {
-        $fields = array( TODO 'fonction_complement', 'personne_denomination_id');
-        $r = xModel::q('DESCRIBE commissions_membres');
-        while ($row = mysql_fetch_assoc($r)) {
-            $result[] = $row['Field'];
-        }
-        return !!array_intersect($result, $fields);
+        $r = xModel::q("SHOW TABLES LIKE 'commissions_propositions_nominations'");
+        return mysql_num_rows($r);
     }
+
 }
 
 new iafbmIssue9();
