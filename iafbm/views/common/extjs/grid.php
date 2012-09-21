0228cfb5ae7e30ca61ff08e119f5600afce45c62
Damien Corpataux <d@mien.ch>
2012-09-21 13:27:06 +0200

    extjs/grid view: Fixed grid height to fit paging size

--- iafbm/views/common/extjs/grid.php
+++ iafbm/views/common/extjs/grid.php
@@ -12,7 +12,7 @@ class CommonExtjsGridView extends CommonExtjsBaseView {
             'title' => 'Elements',
             'id' => sha1(microtime().session_id()),
             'pageSize' => 25,
-            'height' => 605,
+            'height' => 630,
             'columns' => @$this->data['columns'] ? $this->data['columns'] : "iafbm.columns.{$this->data['model']}",
             'editable' => true,
             'autoSync' => false,
