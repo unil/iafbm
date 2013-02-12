e8f2fffd12d0f4239dfb68b5c6ddb14df7046fd6
Damien Corpataux <d@mien.ch>
2013-02-12 18:26:32 +0100

    Effectif reporting: correct count

--- iafbm/controllers/report.php
+++ iafbm/controllers/report.php
@@ -7,7 +7,31 @@ class ReportController extends iaExtRestController {
     }
 
     function effectifCorpsEnseignantAction() {
-        // Groups definition
+        // If all regexps are present in array,
+        // returns true and deletes matching cells,
+        // else, returns false and does nothing
+        $array_preg = function($regexps, &$array) {
+            $array_copy = $array;
+            $matches = array();
+            $regexps = xUtil::arrize($regexps);
+            foreach ($regexps as $i => $regexp) {
+                $m = preg_grep($regexp, $array_copy);
+                if ($m===false) throw new xException("Error with regexp: {$regexp}");
+                $matches = array_merge($matches, $m);
+                // Deletes matched regexp
+                if ($m) unset($regexps[$i]);
+                // Deletes matched cells
+                foreach ($m as $k => $v) unset($array_copy[$k]);
+            }
+            // Regexps array not empty means that some didn't match
+            if ($regexps) return false;
+            // Modifies original array
+            $array = $array_copy;
+            // returns true
+            return true;
+        };
+        // Defines output table contents, using structure
+        // header => array(items)
         $grouping = array(
             "Corps Professoral" => array(
                 'PO', 'PAS', 'PAST'
@@ -19,31 +43,53 @@ class ReportController extends iaExtRestController {
                 'PD', 'PTIT', 'PI', 'CC'
             )
         );
-        // Determines 'activites' ids to query
-        $activites = array();
-        foreach ($grouping as $items) $activites = array_merge($activites, $items);
-        $activites = array_unique($activites);
-        $activites_ids = array();
+        // Defines activites-labels => regexp-expression.
+        // Note: order matters because matching cells will be deleted
+        $items = array(
+            'PD/MER1' => array('/^PD$/', '/^MER1/'),
+            'PD/MER2' => array('/^PD$/', '/^MER2/'),
+            'PO' => '/^PO/',
+            'PAST' => '/^PAST/',
+            'PAS' => '/^PAS/',
+            'MER1' => array('/^MER1/'),
+            'MER2' => array('/^MER2/'),
+            'MA' => '/^MA/',
+            'PD' => array('/^PD/'),
+            'PTIT' => '/^PTIT/',
+            'PI' => '/^PI/',
+            'CC' => '/^CC/',
+        );
+        // Counts activites personne-by-personne
+        $personnes = xModel::load('personne', array(
+            'actif' => 1,
+            'personne_type_id' => 4,
+            'personne_type_id_comparator' => '!=',
+            'xreturn' => 'id',
+            'xjoin' => ''
+        ))->get();
+        $personnes_ids = array_map(function($item) { return $item['id']; }, $personnes);
         //
-        foreach (array('SSC','SSF') as $section) {
-            foreach ($activites as $activite) {
-                $r = xModel::load('activite', array(
-                    'activite_nom_abreviation' => $activite,
-                    'section_code' => $section
-                ))->get(0);
-                if (@$r['id']) $activites_ids[$section][$activite] = $r['id'];
-            }
-        }
-        // Queries database
         $counts = array();
-        foreach ($activites_ids as $section => $activites) {
-            foreach ($activites as $activite => $id) {
-                $count = xModel::load('personne_activite', array(
-                    'activite_id' => $id,
-                    'xjoin' => ''
-                ))->count();
-                // Stores count in data structure
-                $counts[$section][$activite] = $count;
+        foreach ($personnes_ids as $id) {
+            foreach (array('SSF', 'SSC') as $section) {
+                // Fetches personne activites
+                $result = xModel::load('personne_activite', array(
+                    'personne_id' => $id,
+                    'section_code' => $section,
+                    'xreturn' => 'section_code,activite_nom_abreviation'
+                ))->get();
+                // Creates a flat activites array
+                $activites = array();
+                foreach ($result as $activite) $activites[] = $activite['activite_nom_abreviation'];
+                $activites = array_unique($activites);
+                // Counts activites
+                foreach ($items as $item => $regexps) {
+                    if ($array_preg($regexps, $activites)) {
+                        @$counts[$section][$item]++;
+                    }
+                }
+                // TODO: Cellule "Autres" dans le(s) tableaux de report?
+                //if ($activites) throw new xException('Some activites were not counted', 500, $activites);
             }
         }
         // Programatically creates per activite sum (SSC+SSF)
