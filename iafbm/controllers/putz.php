749526c5fd8a39604a706fc0f3f66da5797ec539
Damien Corpataux <d@mien.ch>
2013-03-13 14:28:10 +0100

    Putz: Updated columns order

--- iafbm/controllers/putz.php
+++ iafbm/controllers/putz.php
@@ -8,8 +8,8 @@ class PutzController extends iaExtRestController {
             'personne_prenom' => 'Prénom',
             'personne_nom' => 'Nom',
             'section_code' => 'Section',
-            'activite_type_nom' => "Type d'activité",
             'rattachement_abreviation' => 'Rattachement',
+            'activite_type_nom' => "Type d'activité",
             'activite_nom_abreviation' => 'Activite',
             'fin' => 'Echéance'
         );
